35a39b8cea18bf88e546e45d46ab17c6cd485128
Ian <ikennedy@uw.edu>
2018-04-19 18:05:09 -0700

    worked on lda_output module

--- lda_output.py
+++ lda_output.py
@@ -14,26 +14,40 @@ def get_formatted_topic_list(model, formatting, n_topics=10):
     if formatting == "keywords":
         topics = ["Top keywords are: "+re.sub(r'\s+', ', ',re.sub(r'\d|\W',' ',x[1]))[2:-2] for x in model.print_topics(n_topics,20)]
     return(topics)
-# given some stratifier, compare the topic distributions
-#Count the occurence of each topic by high_white
-no_text = df[list(range(n_topics))].join(df.high_white)
-no_text[no_text<.01] = 0
-no_text = np.sign(no_text)
-#I've run this with means too, but I'll eventually want to do a chi-squared test
-#so I think counts are better
-all = no_text.mean()
-high = no_text[no_text.high_white==1].mean()
-low = no_text[no_text.high_white==0].mean()
 
-#make a df of that data
-mean_diff = pd.DataFrame(data=[all, high, low]).transpose().rename(columns = {0:'all_r', 1:'high_white', 2:'low_white'})
-topic_comparison = pd.DataFrame(data=[all.drop('high_white').sort_values(ascending=False).index, high.drop('high_white').sort_values(ascending=False).index, low.drop('high_white').sort_values(ascending=False).index]).transpose().rename(columns = {0:'all_r', 1:'high_white', 2:'low_white'})
-#calculate the absolute value of the difference between the two categories)
-mean_diff['difference']  = abs(mean_diff.high_white - mean_diff.low_white)
-mean_diff['prop'] = abs(mean_diff.high_white/mean_diff.low_white)
-mean_diff.sort_values(by='prop', ascending=False)
-topic_comparison
-sorted_mean = mean_diff.sort_values('prop', ascending=False)[1:]
+#this function takes a df that has the texts, the toipc distributions, and the stratifieer
+#if topic_cols isn't specified, defaults to list(range(n_topics))
+def summarize_on_stratifier(df_merged, n_topics, stratifier, topic_cols=none, thres = 0.01, method="mean"):
+    # given some stratifier, compare the topic distributions
+    #Count the occurence of each topic by high_white
+    if not(topic_cols):
+        topic_cols = list(range(n_topics))
+    # make a limted df of just the topic_cols and the stratifier
+    no_text = df_merged[topic_cols].join(stratifier)
+    # makes the distributions binary based on the threshold, 1% by default
+    no_text[no_text<.01] = 0
+    no_text = np.sign(no_text)
+    #I've run this with means too, but I'll eventually want to do a chi-squared test
+    #so I think counts are better
+    if method == "mean":
+        all = no_text.mean()
+        high = no_text[stratifier==1].mean()
+        low = no_text[stratifier==0].mean()
+    if method == "sum":
+        all = no_text.sum()
+        high = no_text[stratifier==1].sum()
+        low = no_text[stratifier==0].sum()
+    #make a df of that data
+    mean_diff = pd.DataFrame(data=[all, high, low]).transpose().rename(columns = {0:'all_r', 1:'high_white', 2:'low_white'})
+    #calculate the absolute value of the difference between the two categories)
+    mean_diff['difference']  = abs(mean_diff.high_white - mean_diff.low_white)
+    mean_diff['prop'] = abs(mean_diff.high_white/mean_diff.low_white)
+    mean_diff.sort_values(by='prop', ascending=False)
+    topic_comparison
+    sorted_mean = mean_diff.sort_values('prop', ascending=False)[1:]
+
+    topic_comparison = pd.DataFrame(data=[all.drop('high_white').sort_values(ascending=False).index, high.drop('high_white').sort_values(ascending=False).index, low.drop('high_white').sort_values(ascending=False).index]).transpose().rename(columns = {0:'all_r', 1:'high_white', 2:'low_white'})
+
 
 # given some stratifier, fit an RFC model to identify topic importance
 #do a quick Random Forest Classification to see which topics are most useful for distinguishing
